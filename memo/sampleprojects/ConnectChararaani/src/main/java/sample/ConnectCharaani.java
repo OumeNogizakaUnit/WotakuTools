cfd513a96dda58cf501d27191ede63f2200fd997
shun <shun@osstech.co.jp>
2017-07-31 17:12:17 +0900

    ConnectCharaani 途中

--- memo/sampleprojects/ConnectChararaani/src/main/java/sample/ConnectCharaani.java
+++ memo/sampleprojects/ConnectChararaani/src/main/java/sample/ConnectCharaani.java
@@ -24,20 +24,47 @@ class ConnectCharaani{
 
         System.out.println(cookies);
         System.out.println(ConnectCharaani.BASEURI);
-        Connection.Response response = Jsoup.connect(ConnectCharaani.LOGINURI)
-            .data("txID", "20390084")
-            .data("txPASSWORD", "kawaishun")
-            .method(Method.POST)
-            .execute();
+        Connection con = Jsoup.connect(ConnectCharaani.LOGINURI)
+            .data("ScriptManager1", "ScriptManager1|btnLogin")
+            .data("txID", "userID")
+            .data("txPASSWORD", "pass")
+            .data("btnLogin.x", "-612")
+            .data("btnLogin.y", "-548")
+            .header("X-MicrosoftAjax", "Delta=true")
+            .header("Host", "akb48.chara-ani.com")
+            .header("Referer", "https://akb48.chara-ani.com/login.aspx")
+            .header("Accept-Encoding", "gzip, deflate, br")
+            .userAgent("Mozilla")
+            .method(Method.POST);
+        System.out.println("\n***Request headers***");
+        System.out.println(con.request().headers());
+
+        Connection.Response response = con.execute();
+        System.out.println("\n***Response headers***");
         System.out.println(response.headers());
-        System.out.println(response.cookies());
+        String sessionId = response.cookies().get("ASP.NET_SessionId");
+        System.out.println(sessionId);
 
         System.out.println(ConnectCharaani.HISTORYURI);
-        Document doc = Jsoup.connect(ConnectCharaani.HISTORYURI)
-            .cookies(response.cookies())
-            .get();
+        Connection history = Jsoup.connect(ConnectCharaani.HISTORYURI)
+            .header("Referer", "https://akb48.chara-ani.com/top.aspx")
+            .header("Host", "akb48.chara-ani.com")
+            .header("Accept-Encoding", "gzip, deflate, br")
+            .header("Cookie", "ASP.NET_SessionId=" + sessionId)
+            .cookie("ASP.NET_SessionId", sessionId)
+            .method(Method.GET);
 
-        System.out.println(doc.html());
+        System.out.println("\n***Request headers***");
+        System.out.println(history.request().headers());
+        System.out.println(history.request().cookies());
+
+        Connection.Response resHistory = history.execute();
+        System.out.println("\n***Response headers***");
+        System.out.println(resHistory.headers());
+        System.out.println(resHistory.cookies().get("ASP.NET_SessionId"));
+
+        System.out.println("\n*********\n\n");
+        System.out.println(resHistory.body());
 
         }catch(IOException e) {
         System.out.println(e);
